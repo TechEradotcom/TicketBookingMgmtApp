a47c2e177bbe673f10457bef7a79f01336759ed5
TechEradotcom <sudhirjava6@gmail.com>
2020-08-12 22:57:51 +0530

    code added

---
+++ src/main/java/com/techera/TicketMgmtApp/TicketMgmtAppApplication.java
@@ -0,0 +1,13 @@
+package com.techera.TicketMgmtApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMgmtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techera/TicketMgmtApp/TicketMgmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techera.TicketMgmtApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMgmtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
